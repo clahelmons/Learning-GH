4cab74aed76bf78eeaf55d6c2b729ea2ccf9c3b3
C.L.A. Helmons <cla.helmons@hotmail.com>
2021-05-22 15:13:53 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Kees on 22/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
